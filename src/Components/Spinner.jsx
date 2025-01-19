f841ef1ed33de3ee6bf663a79b232166c5b6c0a0
mhdshefin <rishnabinthsalam@gmail.com>
2025-01-19 21:15:21 +0530

    spinner added

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { ClipLoader } from "react-spinners";
 
+const cssOfSpinner = {
+  borderWidth: "5px",
+}
+
 const Spinner = () => {
   return (
-    <div className='w-full h-screen bg-[#ffffff91] flex items-center justify-center fixed z-50'>
-        <ClipLoader color="tomato" size={70}/>
+    <div className='w-screen h-screen bg-[#ffffff91] flex items-center justify-center fixed z-50'> 
+        <ClipLoader color="tomato" size={window.innerWidth<400?120:150} cssOverride={cssOfSpinner}/>
     </div>
   )
 }
